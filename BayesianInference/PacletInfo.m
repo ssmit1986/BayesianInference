5c2fabcefb18f321b9a936bb22566a8b5c861639
Kuba <kuba.pod@gmail.com>
2019-07-17 14:52:33 +0200

    feat: paclet build

---
+++ BayesianInference/PacletInfo.m
@@ -0,0 +1,34 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2018/11/23*)
+
+Paclet[
+    Name -> "BayesianInference",
+    Version -> "0.0.1",
+    MathematicaVersion -> "11.3+",
+    Creator -> "Sjoerd Smit",
+    Extensions -> 
+        {
+            {
+                "Documentation",
+                Language -> "English"
+            },
+            {
+                "Kernel",
+                "Context" -> "BayesianInference`"(*{
+                    "BayesianUtilities`",
+                    "BayesianStatistics`",
+                    "BayesianGaussianProcess`",
+                    "BayesianVisualisations`",
+                    "BayesianNeuralNetworks`",
+                    "ExponentialFamilyDefinitions`",
+                    "BayesianConjugatePriors`",
+                    "BayesianLinearRegression`"
+                }*)
+            }
+        }
+]
+
+
